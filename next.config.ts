8f15cf826855c790cbd562c9d008bd2839511ccd
abderrahm4ne <abderrahmaneita@gmail.com>
2026-08-14 21:19:30 +0100

    feat:post meta webhook is wokring still need to add agent tools

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['abiding-walk-protrude.ngrok-free.dev'],
 };
 
 export default nextConfig;
